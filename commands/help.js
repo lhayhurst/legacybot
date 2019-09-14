f145c3a7d71dddb01ceed8a685dfc967909d9314
Lyle Hayhurst <lyle.hayhurst@aquatic.com>
2019-09-13 20:39:16 -0700

    added playbooks to help.

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,27 @@
 const {Command} = require('discord-akairo');
 const Discord = require('discord.js');
+const FamilyPlaybook = require('../family_playbook');
 
 class HelpCommand extends Command {
     constructor() {
         super('help command', {
             aliases: ['help', 'h'],
-            allowMention: true
+            allowMention: true,
+            args: [
+                {
+                    id: 'playbooks',
+                    match: 'flag',
+                    prefix: '-p',
+                    default: null
+                },
+            ],
         });
     }
 
     exec(message, args) {
+        if( args.playbooks ) {
+            return message.reply(`Here are some playbooks you can use. You can also make up your own! ${JSON.stringify(Object.keys(FamilyPlaybook.playbooks()))}.`)
+        }
         let richEmbed = new Discord.RichEmbed();
         richEmbed.attachFiles(['assets/commands.png']).setImage("attachment://commands.png")
         return message.reply(richEmbed);
