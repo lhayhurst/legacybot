5c280570f2ed7ce7c1d42da5ab981431c860c3c2
Lyle Hayhurst <lyle.hayhurst@aquatic.com>
2019-09-17 19:39:30 -0700

    added set family name

--- model/fplaybook.js
+++ model/fplaybook.js
@@ -3,7 +3,62 @@ const FamilyPlaybook = require('../family_playbook');
 const PlaybookSchema = require('./playbook');
 const extendSchema = require('./extend_schema');
 
-const FPlaybookSchema = extendSchema(PlaybookSchema, {});
+const FamilySchema = {
+    doctrine: {
+        type: String,
+        default: null
+    },
+    lifestyle: {
+        type: String,
+        default: null,
+    },
+    traditions: {
+        type: [String],
+        default: undefined,
+    },
+    moves: {
+        type: [String],
+        default: undefined
+    },
+    notes: {
+        type: String,
+        default: null
+    },
+    grasp: {
+        type: Number,
+        default: 0
+    },
+    reach: {
+        type: Number,
+        default: 0
+    },
+    sleight: {
+        type: Number,
+        default: 0
+    },
+    data_resource: {
+        type: Number,
+        default: 0
+    },
+    tech: {
+        type: Number,
+        default: 0
+    },
+    needs: {
+        type: [String],
+        default: undefined,
+    },
+    surpluses: {
+        type: [String],
+        default: undefined
+    },
+    treaties: {
+        type: Map,
+        of: String
+    },
+};
+
+const FPlaybookSchema = extendSchema(PlaybookSchema, FamilySchema);
 
 FPlaybookSchema.pre('save', function(next) {
 
